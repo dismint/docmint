f8667cfa966c199747b90954214a45ca3226ee8c
dismint <dismintjjc@gmail.com>
2024-03-14 12:42:32 -0400

    fish script to automatically link template files

---
+++ link.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+# Define the top-level file and the target subdirectory
+set source "/home/dismint/docmint/template.typ"
+set biology "/home/dismint/docmint/biology"
+set religion "/home/dismint/docmint/religion"
+set compilers "/home/dismint/docmint/compilers"
+set security "/home/dismint/docmint/security"
+set multicore "/home/dismint/docmint/multicore"
+set networks "/home/dismint/docmint/networks"
+
+# Create the symbolic link in the target subdirectory
+ln -s $source $biology/(basename $source)
+ln -s $source $religion/(basename $source)
+ln -s $source $compilers/(basename $source)
+ln -s $source $security/(basename $source)
+ln -s $source $multicore/(basename $source)
+ln -s $source $networks/(basename $source)
+
+echo -e "\n== finished linking template.typ =="
